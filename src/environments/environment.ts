2a4281669190ea8aa13c54f5a91e66ee805b73cd
wagaane <ablayefaye9725@gmail.com>
2026-06-09 12:44:15 +0000

    refact

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
  */
 export const environment = {
   production: true,
-  apiUrl: 'http://164.132.43.247/api',
+  apiUrl: 'https://api-sen-stocksaas.com/api',
   appName: 'Stock SaaS',
   appVersion: '1.0.0',
   enableLogging: true
